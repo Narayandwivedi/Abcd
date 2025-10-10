efe9c9867f6c74a40a3494761e851f51e353aa6e
Narayandwivedi <desinplus1@gmail.com>
2025-10-10 19:04:26 +0530

    change backend url and fix cors error

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,9 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'http://localhost:5174',
-    'http://localhost:5175'
+    'http://localhost:5175',
+    'https://abcdvyapar.com',
+    'https://www.abcdvyapar.com'
   ],
   credentials: true, // Allow cookies to be sent
   optionsSuccessStatus: 200
