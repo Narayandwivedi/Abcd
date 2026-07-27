76c5b34d4adba843c29df9eee00e2d691753db72
Narayandwivedi <desinplus1@gmail.com>
2026-07-27 07:58:48 +0530

    make ui better

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -188,7 +188,7 @@ const Footer = ({ noBottomNav = false }) => {
           </div>
         </div>
 
-        <div className={`border-t border-gray-700 mt-8 pt-6 text-center text-gray-400 ${noBottomNav ? 'text-[10px] md:text-xs' : 'text-sm'}`}>
+        <div className={`border-t border-gray-700 mt-3 pt-3 md:mt-8 md:pt-6 text-center text-gray-400 ${noBottomNav ? 'text-[10px] md:text-xs' : 'text-sm'}`}>
           <p>&copy; {new Date().getFullYear()} ABCD. All rights reserved. 
             <a 
               href="https://Softwarebytes.in" 
